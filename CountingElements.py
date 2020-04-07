8f0fe9cd7c0425c797db7fbad0310d07c57944e0
Sharma <mohitsharma16@kpmg.com>
2020-04-08 01:08:23 +0530

    Day 7

---
+++ CountingElements.py
@@ -0,0 +1,21 @@
+"""
+Given an integer array arr, count element x such that x + 1 is also in arr.
+
+If there're duplicates in arr, count them seperately.
+
+Input: arr = [1,1,3,3,5,5,7,7]
+Output: 0
+Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+
+Input: arr = [1,3,2,3,5,0]
+Output: 3
+Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+"""
+
+class Solution:
+    def countElements(self, arr):
+        count = 0
+        for i in arr:
+            if i+1 in arr:
+                count+=1
+        return count
